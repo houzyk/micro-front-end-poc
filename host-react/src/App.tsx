fbfeda55733ac02426c219e7a065fef440fb8658
Houzair Koussa <houzairmk@icloud.com>
2023-02-11 19:23:54 +0400

    [dev] ADD vue wrapper on react app

--- host-react/src/App.tsx
+++ host-react/src/App.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
 
 import RemoteReactApp from "remote_react/RemoteReactApp";
+import VueWrapper from "remote_vue3/Wrapper";
 
-const App = () => (
-  <div className="container">
-    <div>Name: host-react</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-    <RemoteReactApp />
-  </div>
-);
+const App = () => {
+
+  const vueAppRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    VueWrapper(vueAppRef.current);
+  }, [vueAppRef]);
+
+  return (
+    <div className="container">
+      <div>Name: host-react</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Empty CSS</div>
+      <RemoteReactApp />
+      <div ref={vueAppRef}/>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
